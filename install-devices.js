e12ff80c954a9eb5f0a24ce838c29c015c2d5eb1
Michael Balders <michael.balders@taxi.ca>
2018-06-01 14:14:23 -0400

    bum

--- install-devices.js
+++ install-devices.js
@@ -6,7 +6,6 @@ const devices = packageFile.neeoDevices || {};
 console.log("Installing devices...")
 
 for (d in devices){
-	console.log("Installing... " + d);
 	execSync("git clone " + devices[d] + " devices/" + d);
 	execSync("npm install devices/" + d + "  --prefix ./devices/" + d);
 }
